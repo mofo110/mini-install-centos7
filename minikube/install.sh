e96f057bf87e1f7ae0a2d30fc8456878c3f5a6f2
morton <morton@centos7.workgroup>
2018-09-12 23:40:48 -0400

    Add wget minikube command

--- minikube/install.sh
+++ minikube/install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+wget https://github.com/kubernetes/minikube/releases/download/v0.28.2/minikube-linux-amd64
 chmod +x minikube-linux-amd64
 mv minikube-linux-amd64 /usr/local/bin/minikube
